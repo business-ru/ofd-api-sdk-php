7004de8233819f0972a5a7d9b73fcf33a4957c8f
a.nikitin <a.nikitin@mail.business.ru>
2021-06-25 09:09:28 +0500

    Refactoring code

---
+++ src/Codeception/Exception/BruApiException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Codeception\Exception;
+
+use PHPUnit\Framework\AssertionFailedError;
+
+/**
+ * Исключение для модуля BruApiException
+ * Class BruApiException
+ * @package Codeception\Module
+ */
+class BruApiException extends AssertionFailedError
+{
+
+}
