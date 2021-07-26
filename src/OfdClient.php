8b5b63cb8d9eb981b56875a3d7d2ba040622f744
a.nikitin <a.nikitin@mail.business.ru>
2021-07-26 17:23:28 +0500

    Refactoring code

--- src/OfdClient.php
+++ src/OfdClient.php
@@ -2,141 +2,85 @@
 
 namespace Ofd\Api;
 
-use JsonException;
-use Ofd\Api\Http\Request;
-use Ofd\Api\Http\Stream;
-use Ofd\Api\Http\Uri;
-use Psr\Http\Client\ClientExceptionInterface;
-use Psr\Http\Client\ClientInterface;
-use Psr\SimpleCache\CacheInterface;
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
-use Psr\Log\LogLevel;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-final class OfdClient implements LoggerAwareInterface
+/**
+ * Class OfdClient - SDK Ofd API
+ * @package Ofd\Api
+ */
+final class OfdClient
 {
-	use LoggerAwareTrait;
-
 	private const STATIC_URL = "https://testapi.ofd-ya.ru/ofdapi/v1/";
 
 	/**
-	 * @var object
-	 * Объект для работы с кэшем
+	 * Предоставляет гибкие методы для синхронного или асинхронного запроса ресурсов HTTP.
+	 * @var HttpClientInterface|null
 	 */
-	protected $cache;
+	private ?HttpClientInterface $client;
 
 	/**
-	 * @var ClientInterface
-	 *  Http - клиент
+	 * SymfonyHttpClient constructor.
+	 * Токен в Ofd API бессрочный
+	 * @param HttpClientInterface|null $client - Symfony http клиент
 	 */
-	private $httpClient;
-
-	/**
-	 *
-	 * @param CacheInterface|null $cache Объект для кэширования
-	 * @param ClientInterface|null $httpClient HTTP - клиент
-	 */
-	public function __construct(CacheInterface $cache = null, ClientInterface $httpClient = null)
+	public function __construct(HttpClientInterface $client = null)
 	{
-		$this->cache = $cache ?? new SimpleFileCache();
-		$this->httpClient = $httpClient ?? new SimpleHttpClient();
+		# HttpClient - выбирает транспорт cURL если расширение PHP cURL включено,
+		# и возвращается к потокам PHP в противном случае
+		# Добавляем в header токен из cache
+		$this->client = $client ?? HttpClient::create(
+				[
+					'http_version' => '2.0',
+					'headers' => [
+						'Content-Type' => 'application/json',
+						'Ofdapitoken' => getenv('OFD_TOKEN')
+					]
+				]
+			);
 	}
 
 	/**
-	 * Отправить запрос HTTP - клиентом
-	 *
-	 * @param string $model Модель
-	 * @param string $method Метод (get, post, put, delete)
-	 * @param array $params Параметры
-	 * @return mixed
-	 * @throws JsonException|ClientExceptionInterface
-	 */
-	private function sendRequest(string $method, string $model, array $params = [])
-	{
-		#Верхний регистр для метода
-		$method = strtoupper($method);
-		#Создаем объект
-		$request = new Request();
-		#Создаем объект
-		$uri = new Uri();
-		#Создает объект потока, куда будет попадать request body
-		$stream = new Stream('php://temp/ofd/request', 'w+');
-		#Создаем ссылку
-		$uri = $uri->withPath(self::STATIC_URL . $model);
-
-		$request = $request->withRequestTarget(self::STATIC_URL . $model);
-		$request = $request->withMethod($method);
-
-		$paramsString = json_encode($params, JSON_THROW_ON_ERROR);
-
-		if ($method === 'GET') {
-			$uri = $uri->withQuery($paramsString);
-		} else {
-			$stream->write($paramsString);
-		}
-
-		$stream->seek(0);
-
-		$request = $request->withBody($stream);
-		$request = $request->withUri($uri);
-
-		$response = $this->httpClient->sendRequest($request);
-
-		$status_code = $response->getStatusCode();
-
-		$response->getBody()->seek(0);
-
-		$result = $response->getBody()->getContents();
-
-		if ($status_code === 200) {
-			$this->log(LogLevel::INFO, 'Запрос выполнен успешно', ['method' => $method, 'model' => $model, 'params' => $params]);
-			return json_decode($result, true, 2048, JSON_THROW_ON_ERROR);
-		}
-
-		if ($status_code === 401) {
-			$this->log(LogLevel::INFO, 'Токен просрочен');
-			return 401;
-		}
-
-		if ($status_code === 503) {
-			$this->log(LogLevel::INFO, 'Превышен лимит запросов');
-			return 503;
-		}
-		return ["status" => "error", "error_code" => "http:" . $status_code];
-	}
-
-	/**
-	 * @param string $method Метод
-	 * @param string $model Модель
-	 * @param array $params Параметры
-	 * @return int|mixed|string[]
+	 * Метод позволяет выполнить запрос к Open API
+	 * @param string $method - Метод
+	 * @param string $model - Модель
+	 * @param array $params - Параметры
+	 * @return array|string
 	 * @throws \JsonException
-	 * @throws \Psr\Http\Client\ClientExceptionInterface
+	 * @throws ClientExceptionInterface
+	 * @throws DecodingExceptionInterface
+	 * @throws RedirectionExceptionInterface
+	 * @throws ServerExceptionInterface
+	 * @throws TransportExceptionInterface
 	 */
 	public function request(string $method, string $model, array $params = [])
 	{
-		$result = $this->sendRequest(strtoupper($method), $model, $params);
-		//Токен не прошел
-		if ($result === 401) {
-			$result = $this->sendRequest($method, $model, $params);
+		#Создаем ссылку
+		$url = self::STATIC_URL . $model;
+		#Отправляем request запрос
+		$response = $this->client->request(
+			strtoupper($method),
+			$url,
+			[
+				'body' => json_encode($params, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+			]
+		);
+		#Получаем статус запроса
+		$statusCode = $response->getStatusCode();
+		if ($statusCode === 200) {
+			return json_decode(
+				$response->getContent(false),
+				true,
+				512,
+				JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT
+			);
 		}
-		return $result;
+		#false - убрать throw от Symfony.....
+		return $response->toArray(false);
 	}
-
-	/**
-	 * Форматирует сообщение для логирования и записывает в лог,
-	 * если был установлен логгер через метод setLogger()
-	 * @param string $level Уровень важности
-	 * @param string $message Сообщение
-	 * @param array $context Контекст сообщения
-	 */
-	private function log(string $level, string $message, array $context = []): void
-	{
-		if ($this->logger) {
-			$messageF = date("Y-m-d H:i:s") . ': ' . trim($message, '.') . '.' . PHP_EOL;
-			call_user_func([$this->logger, $level], $messageF, $context);
-		}
-	}
-
-
 }
